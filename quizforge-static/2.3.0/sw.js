041470e1fcab41005c714befe2fd002ee3f8acc5
EMDEV <12120674+eliamure@users.noreply.github.com>
2026-07-23 19:25:09 +0200

    fix(quizforge): precache complete offline application

--- quizforge-static/2.3.0/sw.js
+++ quizforge-static/2.3.0/sw.js
@@ -1,5 +1,5 @@
 const CACHE='quizforge-2.3.0';
-const CORE=['./','./index.html','./app.js','./manifest.webmanifest'];
+const CORE=['./','./index.html','./core.js','./age10.js','./age13.js','./age16.js','./age20.js','./engine.js','./app.js','./manifest.webmanifest'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(CORE)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',event=>{if(event.request.method!=='GET')return;event.respondWith(caches.match(event.request).then(hit=>hit||fetch(event.request).then(response=>{const copy=response.clone();caches.open(CACHE).then(cache=>cache.put(event.request,copy));return response}).catch(()=>caches.match('./index.html'))))});
